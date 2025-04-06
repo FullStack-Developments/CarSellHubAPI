1fae1f748332bf2abb9eb4be0617cb74c8755c26
Jafar Marouf <jafar.k.marouf@gmail.com>
2025-04-07 01:42:33 +0300

    create custome response for api

---
+++ app/Traits/ApiResponseTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponseTrait
+{
+    public function sendSuccess($data, $message, $code = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'data' => $data,
+            'message' => $message,
+        ], $code);
+    }
+    public function sendError($message, $code = 500): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'data' => [],
+            'message' => $message,
+        ], $code);
+    }
+
+    public function sendValidationError($message, $code = 422): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'data' => [],
+            'message' => $message,
+        ], $code);
+    }
+}
